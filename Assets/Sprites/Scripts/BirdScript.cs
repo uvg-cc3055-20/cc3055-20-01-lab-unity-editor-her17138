2bcecbcf0044f9d31fae60a01fd42988a51cd0cc
her17138 <her17138@uvg.edu.gt>
2018-01-17 09:21:30 -0600

    miércoles 9:21am, el pajarito salta

---
+++ Assets/Sprites/Scripts/BirdScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+	public float JumpForce = 80f;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+		rb = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			rb.velocity = Vector2.zero;
+			rb.AddForce (Vector2.up * JumpForce);
+		}
+
+	}
+}
