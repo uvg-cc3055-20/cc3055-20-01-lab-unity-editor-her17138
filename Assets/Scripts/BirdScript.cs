93bfbd1027ba50510573e5a16066463599c989c3
her17138 <her17138@uvg.edu.gt>
2018-01-17 09:36:47 -0600

    wed 9:36am

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BirdScript : MonoBehaviour {
-	public float JumpForce = 80f;
+	public float JumpForce = 150f;
 	Rigidbody2D rb;
+	public float forwardSpeed = 2f;
+	public GameObject cam;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +21,8 @@ public class BirdScript : MonoBehaviour {
 			rb.velocity = Vector2.zero;
 			rb.AddForce (Vector2.up * JumpForce);
 		}
+		rb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed* Time.deltaTime);
+		cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed* Time.deltaTime);
 
 	}
 }
